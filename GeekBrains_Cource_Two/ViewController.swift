325c41fe9bb5395fb7f96cb2a88bb36ebfa209e6
Samborsky <samborsky.yaroslav@yandex.ru>
2022-05-12 23:31:52 +0300

    добавил outlet и action и alert
    взаимодействие с основными полями и добавил алерты

--- GeekBrains_Cource_Two/ViewController.swift
+++ GeekBrains_Cource_Two/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var loginTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var loginButton: UIButton!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
+    @IBAction func loginButtonPressing(_ sender: UIButton) {
 
+        guard let login = loginTextField.text,
+        let password = passwordTextField.text else { return }
+
+        if login == "login" && password == "qwerty" {
+            print("Вы вошли")
+            loginTextField.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            passwordTextField.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            //дальше логика, если поля пустые
+        } else if login == "" || password == "" {
+
+            //создаем алерт если поля не заполнены
+            let alertVC =  UIAlertController.init(title: "Ошибка!", message: "Вы не ввели логин или пароль", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertVC.addAction(okAction)
+            present(alertVC, animated: true, completion: nil)
+        }
+        else {
+            print("Неверные данные")
+
+            //создаем алерт, если ошибка в логине/пароле
+            loginTextField.backgroundColor = UIColor.red
+            passwordTextField.backgroundColor = UIColor.red
+            let alertVC =  UIAlertController.init(title: "Ошибка!", message: "Вы ввели неправильные данные, попробуйте еще раз", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertVC.addAction(okAction)
+            present(alertVC, animated: true, completion: nil)
+        }
+    }
+
+}
