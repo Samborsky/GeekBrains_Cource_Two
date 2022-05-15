a0d08371e1e759957c26ea0882fc6087e8669a97
Samborsky <samborsky.yaroslav@yandex.ru>
2022-05-15 22:50:16 +0300

    1

---
+++ GeekBrains_Cource_Two/ViewControllerNextView.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerNextView.swift
+//  GeekBrains_Cource_Two
+//
+//  Created by Samborsky on 15.05.2022.
+//
+
+import UIKit
+
+class ViewControllerNextView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
